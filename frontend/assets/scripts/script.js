3b9870da38c3b8d71cf821c674aeb559913c424c
Alexandrey <alexandre.yucra@outlook.com>
2020-05-06 17:55:03 -0300

    favicon added/ scroll smooth

--- frontend/assets/scripts/script.js
+++ frontend/assets/scripts/script.js
@@ -23,3 +23,13 @@ $(function () {
 
   });
 });
+// $('.nav-container a[href^="#"]').on('click', function(e) {
+//   e.preventDefault();
+  
+//   var id = $(this).attr('href'), 
+//       targetOffset = $(id).offset().top;
+  
+//   $('html, body').animate({ 
+//     scrollTop: targetOffset - 100
+//    }, 500);
+// });
