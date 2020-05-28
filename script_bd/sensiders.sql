ccd482599b1ea0517e0499cc58eecabfce63d2e1
Alexandrey <alexandre.yucra@outlook.com>
2020-05-27 21:20:15 -0300

    modificando o banco

--- script_bd/sensiders.sql
+++ script_bd/sensiders.sql
@@ -3,18 +3,31 @@ use sensiders;
 
 create table Supermercado(
 	idSupermercado int primary key auto_increment,
-    cep char(8) not null,
-    numero int not null,
-    email varchar(50) not null,
-    senha varchar(20) not null,
     nome varchar(30) not null
 );
+
+create table Filial (
+    idFilial int primary key auto_increment,
+    fkSupermercado int not null,
+    foreign key (fkSupermercado) references Supermercado(idSupermercado),
+    cep char(8) not null,
+    numero int not null
+);
+
+create table Usuario (
+	idUsuario int primary key auto_increment,
+    emailUsuario varchar(70) not null unique,
+    senhaUsuario varchar(45) not null,
+    nomeUsuario varchar(45) not null,
+    fkIdFilial int not null,
+    foreign key(fkIdFilial) references Filial(idFilial)
+); 
 
 create table Setor (
 	idSetor int primary key auto_increment,
     nome varchar(30) not null,
-    fkSupermercado int not null,
-    foreign key(fkSupermercado) references Supermercado(idSupermercado)
+    fkFilial int not null,
+    foreign key(fkFilial) references Filial(idFilial)
 );
 
 create table Sensor (
@@ -33,17 +46,32 @@ create table dado (
 );
 
 insert into Supermercado values
-(null, '05386330', 37, 'dia@dia.com', 'dia123', 'Dia'),
-(null, '09240000', 399, 'extra@extra.com', 'extra123', 'Extra');
-
+(null, 'Sonda'),
+(null, 'Dias');
 select * from Supermercado;
+
+insert into Filial values
+-- id    idmarket     nome      email   senha     cep    numero
+(null, 1, 02235001, 1214),
+(null, 1, 02236005, 9242),
+(null, 2, 06624009, 343),
+(null, 2, 07724001, 151);
+
+select * from filial;
+
+-- id   email    senha   nome   fkfilial
+insert into Usuario values
+(null,'joao@outlook.com', 'q1w2e3r4','Gerente Joao', 1),
+(null, 'paulo@outlook.com', 'a1s2d3f4','Administrador',2),
+(null, 'carlinha@htomail.com', 'z1x2c3v4b5','Gerente Carla',3),
+(null, 'carlinhos@htomail.com', 'z1x2c3w1e2','Gerente Carlos',4);
 
 insert into Setor values
 (null, 'Padaria', 1),
 (null, 'Padaria', 2),
 (null, 'Açougue', 2),
-(null, 'Açougue', 1),
-(null, 'Eletrônicos', 2);
+(null, 'Açougue', 4),
+(null, 'Eletrônicos', 3);
 
 select * from setor;
 
@@ -64,8 +92,10 @@ insert into sensor values
 (null, 5),
 (null, 5);
 
+select * from sensor;
+
 select * from setor, sensor where idSetor = fkSetor;
-select * from supermercado, setor where idSupermercado = fkSupermercado and idSupermercado = 1;
+select * from supermercado, setor where idSupermercado = fkFilial and idSupermercado = 1;
 select * from supermercado, setor, sensor where  idSupermercado = fkSetor and idSetor = fkSetor;
 
 insert into dado values('2020-04-05 10:34:09', 1, 's'),
@@ -80,5 +110,7 @@ insert into dado values('2020-04-05 10:34:09', 1, 's'),
 ('2020-04-05 10:34:39', 1, 'n'),
 ('2020-04-05 10:34:39', 2, 'n'),
 ('2020-04-05 10:34:39', 3, 'n');
+
+select * from dado;
 
 select * from dado where dataHora = '2020-04-05 10:34:19';
