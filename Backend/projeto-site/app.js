95ad1e110e750d3d17c03837d112e5f0fc08a8b6
anderson-sartorio <anderson.ssilva@bandtec.com.br>
2020-07-01 17:31:39 -0300

    Alteração das rotas e gráficos

--- Backend/projeto-site/app.js
+++ Backend/projeto-site/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usuariosRouter = require('./routes/usuarios');
-var leiturasRouter = require('./routes/leituras');
+var dadoRouter = require('./routes/dado');
 
 var app = express();
 
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/usuarios', usuariosRouter);
-app.use('/leituras', leiturasRouter);
+app.use('/dado', dadoRouter);
 
 module.exports = app;
