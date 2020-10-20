c819a184f9f32858a47dd8bd3c0a8d19190cff03
patrick_lessa <patrick.lessa03@outlook.com>
2020-10-20 15:11:22 -0300

    Login Java no azure funcionando

---
+++ Application/projeto-java-monitoramento/src/main/java/model/bean/Maquina.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Patrick L Teixeira
+ */
+public class Maquina extends Thread{
+    private Integer cpu;
+    private Integer mem;
+    private Integer disk;
+    private Integer temp;
+    
+    
+}
