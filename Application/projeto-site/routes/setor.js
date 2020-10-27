aa9f0c13855f0f4ac5929e5adb6ac49b86c51957
Josué S. Pereiera <josue.pereira@bandtec.com.br>
2020-10-27 00:33:26 -0200

    Backend Tela setor - parte 2 (update)

--- Application/projeto-site/routes/setor.js
+++ Application/projeto-site/routes/setor.js
@@ -7,6 +7,7 @@ router.get('/:idFilial', function (req, res, next) {
 	const idFilial = req.params.idFilial;
         console.log(`Recuperando todos os setores da filial ${idFilial}`);
         const instrucaoSql = `SELECT * FROM Setor WHERE fkFilial = ${idFilial};`;
+        
 
         sequelize.query(instrucaoSql, {type: sequelize.QueryTypes.SELECT })
                 .then(resultado => {
@@ -16,5 +17,45 @@ router.get('/:idFilial', function (req, res, next) {
                         res.status(500).send(erro.message);
                 });
 });
+router.get('/all/:idSetor', function (req, res, next) {
+	const idSetor = req.params.idSetor;
+        console.log(`Recuperando todos os dados do Setor ${idSetor}`);
+        const instrucaoSql = `SELECT * FROM Setor WHERE idSetor = ${idSetor};`;
+        
+
+        sequelize.query(instrucaoSql, {type: sequelize.QueryTypes.SELECT })
+                .then(resultado => {
+                        res.json(resultado);
+                }).catch(erro => {
+                        console.error(erro)
+                        res.status(500).send(erro.message);
+                });
+});
+
+// Editar setor
+router.post('/editar/:idSetor', function(req, res, next) {
+	console.log('Editando o setor');
+	
+	const inputEdit1 = req.body.inputEdit1;
+        const inputEdit2 = req.body.inputEdit2;
+        const idSetor = req.params.idSetor;
+        
+
+	let instrucaoSql = `UPDATE setor SET nome = '${inputEdit1}', qtdSensores = '${inputEdit2}' WHERE idSetor = '${idSetor}';`;
+
+	sequelize.query(instrucaoSql).then(resultado => {
+		console.log(`Setor editado: ${resultado}`)
+        res.send(resultado);
+    }).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+  	});
+});
+
+        
+
+
+
+
 
 module.exports = router;
