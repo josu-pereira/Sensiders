88a9e46b31ea1f2d18105fb8189db67be8a1542b
Ubuntu <ubuntu@ip-172-31-91-88.ec2.internal>
2020-11-18 15:30:57 +0000

    opcao de escolha de gui e i3 - script

--- scripts/execgui.bash
+++ scripts/execgui.bash
@@ -12,8 +12,8 @@ if [[ $pckt == "s" ]]
 fi
 
 echo -e "\e[48;5;204m [Sensiders]: \e[0m caso voce esteja numa instancia"
-read -p " Gostaria de instalar uma window manager i3?(s/n)" gui
-if [[ $gui == "s" ]]
+read -p " Gostaria de instalar uma window manager i3 ou uma gui lxde ou nao?(i3/lxde/n)" gui
+if [[ $gui == "i3" ]]
 	then
 		echo -e "\e[48;5;204m [Sensiders]: \e[0m Estaremos instalando os pacotes, aguarde..."
 
@@ -24,7 +24,19 @@ if [[ $gui == "s" ]]
 		sed -i 's/allowed_users=console/allowed_users=ubuntu/g' /etc/X11/Xwrapper.config
 		service xrdp start
 		echo -e "\e[48;5;204m [Sensiders]: \e[0m Ja configurado, agora para se conectar na area remota, conecte com o ip da instancia"
-	else
+elif [[ $gui == "lxde" ]]
+	then
+		echo -e "\e[48;5;204m [Sensiders]: \e[0m Estaremos instalando os pacotes, aguarde..."
+
+		sudo apt install xrdp lxde-core lxde tigervnc-standalone-server -y
+		#sudo apt install xrdp tigervnc-standalone-server xorg i3 i3blocks i3lock terminator acpi xbacklight rofi feh clipit pcmanfm
+		echo -e "\e[48;5;204m [Sensiders]: \e[0m aguarde mais um pouco..."
+		sed -i 's/max_bpp=32/max_bpp=16/g' /etc/xrdp/xrdp.ini
+		sed -i 's/allowed_users=console/allowed_users=ubuntu/g' /etc/X11/Xwrapper.config
+		service xrdp start
+		echo -e "\e[48;5;204m [Sensiders]: \e[0m Ja configurado, agora para se conectar na area remota, conecte com o ip da instancia"
+
+else
 		echo -e "\e[48;5;204m [Sensiders]: \e[0m Tudo bem, vamos continuar"
 		read -p " Gostaria de iniciar o java? (s/n)" java
 	if [[ $java == "s" ]]
