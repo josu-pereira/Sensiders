128a99def36723805434e17dd14ac1f82c3bc6e2
Ubuntu <ubuntu@ip-172-31-37-144.ec2.internal>
2020-11-25 19:52:18 +0000

    script mysql feito

--- scripts/execmysql.bash
+++ scripts/execmysql.bash
@@ -7,13 +7,13 @@ sudo systemctl enable docker
 
 echo -e "\e[48;5;204m [Sensiders]: \e[0m Verificando se voce tem mysql no docker."
 
-docker inspect mysql8 | grep MYSQL_MAJOR
+sudo docker inspect mysql8 | grep MYSQL_MAJOR
 
 if [[ $? = 0 ]]
 	then
 		echo -e "\e[48;5;204m [Sensiders]: \e[0m Vemos que voce ja o mysql no docker baixado"
 	else
-		echo -e "\e[48;5;204m [Sensiders]: \e[0m nao encontramos nenhuma versao do java instalado"
+		echo -e "\e[48;5;204m [Sensiders]: \e[0m nao encontramos nenhuma versao do mysql instalado"
 		read -p "vamos instalar a imagem mysql para voce, voce confirmar?(s/n)" res
 	if [[ $res == "s" ]]
 		then
@@ -23,6 +23,6 @@ if [[ $? = 0 ]]
             echo -e "\e[48;5;204m [Sensiders]: \e[0m Abrindo o terminal da instancia:"
             echo -e "\e[48;5;204m [Sensiders]: \e[0m Senha do mysql: \e[1murubu100"
             sleep 2
-		    sudo docker exec -it $(docker ps -a -q) bash
+		    sudo docker exec -it $(sudo docker ps -a -q) bash
 	fi
 fi
